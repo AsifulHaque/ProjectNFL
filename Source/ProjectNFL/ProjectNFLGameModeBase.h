8db9d3a432dd0a16b4ff1c788363f34d72123d85
Asiful Haque <34242497+AsifulHaque@users.noreply.github.com>
2021-07-24 17:52:45 +0600

    Initial commit

---
+++ Source/ProjectNFL/ProjectNFLGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectNFLGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTNFL_API AProjectNFLGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
